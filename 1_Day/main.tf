c423d5de7898666d2c959747f4c32182c09c344f
Bhargav <bhargav@TheCitadel.>
2024-12-23 17:11:02 +0000

    Blank Commit
    Signed-off-by: Bhargav <bhargav@TheCitadel.>

--- 1_Day/main.tf
+++ 1_Day/main.tf
@@ -1,15 +1,15 @@
-#Define Providers
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-#Define Resource EC2 Instance
 resource "aws_instance" "ec2_instance" {
-    ami_id = "ami-0e2c8caa4b6378d8c"
+    ami = "ami-0e2c8caa4b6378d8c"
     instance_type = t2.micro
+    tags = {
+        Name = "terraform_demo"
+    }
 }
 
-#Define Name for EC2 Instance using tags
-tags {
-    Name = "terraform_demo"
-}
+
+
+
